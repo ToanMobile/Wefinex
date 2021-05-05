684f1bdbdebb45cda88f66948241dc0540883465
VanToan <hvtoan@netacom.vn>
2021-05-05 22:09:38 +0700

    update coin.

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:js';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wefinex/base/base_view_view_model.dart';
 import 'package:wefinex/shared/constant/common.dart';
 
 import 'home_binding.dart';
+import 'market/market_coin_item.dart';
 
 /*
 Created by ToanDev on 02/05/2021
@@ -18,6 +20,8 @@ class HomeScreen extends BaseView<HomeController> {
   Map? globalData;
   List marketSortType = ["MKTCAP", true];
   String filter = "";
+  final String upArrow = "⬆";
+  final String downArrow = "⬇";
 
   @override
   Widget vBuilder() => Scaffold(
@@ -37,7 +41,7 @@ class HomeScreen extends BaseView<HomeController> {
             slivers: <Widget>[
               SliverList(
                   delegate: SliverChildListDelegate(<Widget>[
-                    globalData != null
+                globalData != null
                     ? Container(
                         padding: const EdgeInsets.all(10.0),
                         child: Row(
@@ -56,8 +60,8 @@ class HomeScreen extends BaseView<HomeController> {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: <Widget>[
-                                Text("\$" + normalizeNum(globalData["total_market_cap"]), style: Common().textStyle.styleRegular12White),
-                                Text("\$" + normalizeNum(globalData["total_volume_24h"]), style: Common().textStyle.styleRegular12White),
+                                Text("\$" + normalizeNum(globalData!["total_market_cap"]), style: Common().textStyle.styleRegular12White),
+                                Text("\$" + normalizeNum(globalData!["total_volume_24h"]), style: Common().textStyle.styleRegular12White),
                               ],
                             )
                           ],
@@ -65,7 +69,7 @@ class HomeScreen extends BaseView<HomeController> {
                     : Container(),
                 Container(
                   margin: const EdgeInsets.only(left: 6.0, right: 6.0),
-                  decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Theme.of(context).dividerColor, width: 1.0))),
+                  decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Common().color.darkGray, width: 1.0))),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
@@ -76,20 +80,20 @@ class HomeScreen extends BaseView<HomeController> {
                           } else {
                             marketSortType = ["Name", false];
                           }
-                          setState(() {
+                          /*setState(() {
                             _sortMarketData();
-                          });
+                          });*/
                         },
                         child: Container(
                           padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          width: MediaQuery.of(context).size.width * marketColumnProps[0],
+                          width: ScreenUtil.defaultSize.width * marketColumnProps[0],
                           child: marketSortType[0] == "Name"
-                              ? Text(marketSortType[1] ? "${"currency".tr} " + upArrow : "${"currency".tr} " + downArrow, style: Common().textStyle.styleRegular12White)
-                              : Text("currency".tr, style: Common().textStyle.styleRegular12White),
+                              ? Text(marketSortType[1] ? "${Common().string.currency} " + upArrow : "${Common().string.currency} " + downArrow, style: Common().textStyle.styleRegular12White)
+                              : Text(Common().string.currency, style: Common().textStyle.styleRegular12White),
                         ),
                       ),
                       Container(
-                        width: MediaQuery.of(context).size.width * marketColumnProps[1],
+                        width: ScreenUtil.defaultSize.width * marketColumnProps[1],
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: <Widget>[
@@ -100,15 +104,15 @@ class HomeScreen extends BaseView<HomeController> {
                                   } else {
                                     marketSortType = ["MKTCAP", true];
                                   }
-                                  setState(() {
+                                  /*setState(() {
                                     _sortMarketData();
-                                  });
+                                  });*/
                                 },
                                 child: Padding(
                                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                                   child: marketSortType[0] == "MKTCAP"
-                                      ? Text(marketSortType[1] ? "${"market_cap".tr} " + downArrow : "${"market_cap".tr} " + upArrow, style: Common().textStyle.styleRegular12White)
-                                      : Text("market_cap".tr, style: Common().textStyle.styleRegular12White),
+                                      ? Text(marketSortType[1] ? "${Common().string.market_cap} " + downArrow : "${Common().string.market_cap} " + upArrow, style: Common().textStyle.styleRegular12White)
+                                      : Text(Common().string.market_cap, style: Common().textStyle.styleRegular12White),
                                 )),
                             Text("/", style: Common().textStyle.styleRegular12White),
                             InkWell(
@@ -118,9 +122,9 @@ class HomeScreen extends BaseView<HomeController> {
                                 } else {
                                   marketSortType = ["TOTALVOLUME24H", true];
                                 }
-                                setState(() {
+                                /*setState(() {
                                   _sortMarketData();
-                                });
+                                });*/
                               },
                               child: Padding(
                                 padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -139,17 +143,17 @@ class HomeScreen extends BaseView<HomeController> {
                           } else {
                             marketSortType = ["CHANGEPCT24HOUR", true];
                           }
-                          setState(() {
+                          /*setState(() {
                             _sortMarketData();
-                          });
+                          });*/
                         },
                         child: Container(
                           alignment: Alignment.centerRight,
                           padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          width: MediaQuery.of(context).size.width * marketColumnProps[2],
+                          width: ScreenUtil.defaultSize.width * marketColumnProps[2],
                           child: marketSortType[0] == "CHANGEPCT24HOUR"
-                              ? Text(marketSortType[1] == true ? "${"price_24h".tr} " + downArrow : "${"price_24h".tr} " + upArrow, style: Common().textStyle.styleRegular12White)
-                              : Text("price_24h".tr, style: Common().textStyle.styleRegular12White),
+                              ? Text(marketSortType[1] == true ? "${Common().string.price_24h} " + downArrow : "${Common().string.price_24h} " + upArrow, style: Common().textStyle.styleRegular12White)
+                              : Text(Common().string.price_24h, style: Common().textStyle.styleRegular12White),
                         ),
                       ),
                     ],
@@ -166,13 +170,44 @@ class HomeScreen extends BaseView<HomeController> {
                       )
                     ]))
                   : SliverList(
-                      delegate: SliverChildBuilderDelegate((BuildContext context, int index) => CoinListItem(filteredMarketData[index], marketColumnProps),
+                      delegate: SliverChildBuilderDelegate((BuildContext context, int index) => CoinListItem(filteredMarketData![index], marketColumnProps),
                           childCount: filteredMarketData == null ? 0 : filteredMarketData?.length))
             ],
           ))
       : Container(
           child: Center(child: CircularProgressIndicator()),
         );
+
+  normalizeNum(num? input) {
+    if (input == null) {
+      input = 0;
+    }
+    if (input >= 100000) {
+      return numCommaParse(input.round().toString());
+    } else if (input >= 1000) {
+      return numCommaParse(input.toStringAsFixed(2));
+    } else {
+      return input.toStringAsFixed(6 - input.round().toString().length);
+    }
+  }
+
+  numCommaParse(numString) {
+    final shortenOn = false;
+    if (shortenOn) {
+      String str = num.parse(numString ?? "0").round().toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]},");
+      List<String> strList = str.split(",");
+
+      if (strList.length > 3) {
+        return strList[0] + "." + strList[1].substring(0, 4 - strList[0].length) + "B";
+      } else if (strList.length > 2) {
+        return strList[0] + "." + strList[1].substring(0, 4 - strList[0].length) + "M";
+      } else {
+        return num.parse(numString ?? "0").toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]},");
+      }
+    }
+
+    return num.parse(numString ?? "0").toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]},");
+  }
 
   _filterMarketData() {
     print("filtering market data");
