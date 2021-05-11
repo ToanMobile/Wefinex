dcbf8730be57f71321cb08167e7b704113a67653
Văn Toàn <huynhvantoan.itc@gmail.com>
2021-05-11 21:28:59 +0700

    no message

---
+++ lib/ui/details/coin_details.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:wefinex/base/base_view_view_model.dart';
+import 'package:wefinex/shared/constant/common.dart';
+import 'coin_details_binding.dart';
+
+/*
+Created by ToanDev on 02/05/2021
+Company: Netacom.
+Email: hvtoan.dev@gmail.com
+*/
+
+class HomeScreen extends BaseView<HomeController> {
+
+  @override
+  Widget vBuilder() => Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text("Home"),
+          ),
+        ),
+      );
+}
---
+++ lib/ui/details/coin_details_binding.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:wefinex/base/super_base_controller.dart';
+import 'package:wefinex/repository/model/bong_da_entity.dart';
+
+/*
+Created by ToanDev on 02/05/2021
+Company: Netacom.
+Email: hvtoan.dev@gmail.com
+*/
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => HomeController());
+  }
+}
+
+class HomeController extends SuperBaseController<BongDaEntity> {
+
+  @override
+  void onInit() {
+    super.onInit();
+    logWhenDebug("HomeController", "1");
+   /* MyTranslations.init();
+    logWhenDebug("CURRENT LANGUAGE : ", Get.locale.languageCode.toString());*/
+  }
+
+}
